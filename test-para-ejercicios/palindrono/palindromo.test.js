0f3b07c81adc08cf13deb5d41e8ffffe7cbf724a
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-09-09 13:11:52 +0200

    test palindromo modificaciones

--- test-para-ejercicios/palindrono/palindromo.test.js
+++ test-para-ejercicios/palindrono/palindromo.test.js
@@ -2,13 +2,17 @@
 const esPalindromo=require("./palindromo")
 
 test("Comprobar si la función esPalindromo está definida",()=>{
+    //Comprueba si la función existe
     expect(esPalindromo).toBeDefined()
     }
 )
-test("Es un palindromo",()=>{
-    expect(esPalindromo("otto")).toEqual(true)
+
+test("Que el argumento funcione cuando no es un palindromo",()=>{
+    palabra="victor"
+    expect(esPalindromo(palabra)).toEqual(false)
 })
 
-test("No es un palindromo",()=>{
-    expect(esPalindromo("victor")).toEqual(false)
+test("que el argumento funcione, cuando es palindromo",()=>{
+    palabra="ana"
+    expect(esPalindromo(palabra)).toBe(true)
 })
