77f1bd77a3e57f3375a0ec54e3a2782235dada37
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-11-14 11:24:52 +0100

    fetch API

--- fetchAPI/fetchAPI.js
+++ fetchAPI/fetchAPI.js
@@ -6,29 +6,31 @@ Consultar api en GIPHY y escribir los resultados a través del dom
     - Poner un título e imagen de gif a través del dom
 */
 
-const apiKey="ZciR4qtL1TWi0emeTYBkwuOtp7T3Tyb1"
 
-async function consultarAPI(apiKey){
-    var url=""
-    await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
-    .then(res=>res.json())
-    .then(({data})=>{
-        console.log(data.images.original.url)
-        url=data.images.original.url
-    })
-    .catch(console.warn)
 
-    const seccion=document.querySelector('section')
-    const title=document.createElement('h1')
-    title.textContent=("Prueba de fetch api")
-    title.setAttribute('style','text-align:center')
-    seccion.appendChild(title)
-    const imagen=document.createElement('img')
-    imagen.setAttribute('src',url)
-    imagen.setAttribute('alt',"")
-    imagen.setAttribute('width',"900")
-    imagen.setAttribute('heigth',"900")
-    seccion.appendChild(imagen)
+const consultarAPI=async()=>{
+    try{
+        const apiKey="ZciR4qtL1TWi0emeTYBkwuOtp7T3Tyb1"
+        const res=await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+        const {data}=await res.json()
+        const {url}=data.images.original
+    
+
+        const seccion=document.querySelector('section')
+        const title=document.createElement('h1')
+        title.textContent=("Prueba de fetch api")
+        title.setAttribute('style','text-align:center')
+        seccion.appendChild(title)
+        const imagen=document.createElement('img')
+        imagen.setAttribute('src',url)
+        imagen.setAttribute('alt',"")
+        imagen.setAttribute('width',"900")
+        imagen.setAttribute('heigth',"900")
+        seccion.appendChild(imagen)
+    }catch(error){
+        //manejo del error
+    }
+    
 
 }
-consultarAPI(apiKey)
+consultarAPI()
