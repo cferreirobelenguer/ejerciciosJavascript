67e522f12dc895d2ceb66432f36bfb64a460aed5
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-12-03 01:01:50 +0100

    validarCartas

--- README.md
+++ README.md
@@ -292,6 +292,28 @@ Consultar api en GIPHY y escribir los resultados a través del dom
 
 ## TYPESCRIPT
 
+### typescript validarCartas
+
+El Grinch está abriendo las cartas que iban a Santa Claus y las está dejando hechas un lío. 😱
+
+Las cartas son una cadena de texto que incluyen regalos y paréntesis ().
+
+Para saber si una carta es válida ✅, debes comprobar que los paréntesis cierran correctamente y que, además, no vayan vacíos.
+
+¡Pero ojo! Porque el Grinch ha dejado llaves { y corchetes [ dentro de los paréntesis que hacen que no sean válidas. Por suerte sólo los ha dejado en medio de los paréntesis...
+
+Ejemplos:
+
+"bici coche (balón) bici coche peluche" // -> ✅
+"(muñeca) consola bici" // ✅
+
+"bici coche (balón bici coche" // -> ❌
+"peluche (bici [coche) bici coche balón" // -> ❌
+"(peluche {) bici" // -> ❌
+"() bici" // ❌
+
+Crea una función que pasándole el texto de la carta, devuelva true si es válida y false si no lo es. ¡Y acaba con la travesura del Grinch!
+
 ### typescript contadorOvejas
 
 Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color. Haz una función que devuelva una lista con todas las ovejas que sean de color rojo y que además su nombre contenga tanto las letras n Y a, sin importar el orden, las mayúsculas o espacios.
---
+++ validarCartas/validarCartas.js
@@ -0,0 +1,53 @@
+/*El Grinch está abriendo las cartas que iban a Santa Claus y las está dejando hechas un lío. 😱
+
+Las cartas son una cadena de texto que incluyen regalos y paréntesis ().
+
+Para saber si una carta es válida ✅, debes comprobar que los paréntesis cierran correctamente y que, además, no vayan vacíos.
+
+¡Pero ojo! Porque el Grinch ha dejado llaves { y corchetes [ dentro de los paréntesis que hacen que no sean válidas. Por suerte sólo los ha dejado en medio de los paréntesis...
+
+Ejemplos:
+
+"bici coche (balón) bici coche peluche" // -> ✅
+"(muñeca) consola bici" // ✅
+
+"bici coche (balón bici coche" // -> ❌
+"peluche (bici [coche) bici coche balón" // -> ❌
+"(peluche {) bici" // -> ❌
+"() bici" // ❌
+
+Crea una función que pasándole el texto de la carta, devuelva true si es válida y false si no lo es. ¡Y acaba con la travesura del Grinch!
+
+*/
+var validar = function (carta) {
+    var validada = false;
+    //Tiene que incluir ambos paréntesis para ser validado
+    if (carta.includes("(") && (carta.includes(")"))) {
+        //cojo las posiciones de los paréntesis
+        var posicion1 = carta.indexOf("(");
+        var posicion2 = carta.indexOf(")");
+        //cojo lo de dentro de los paréntesis sin incluir ambos para ver lo que hay dentro
+        var palabra = carta.substring(posicion1 + 1, posicion2);
+        console.log(palabra);
+        if (palabra != "") {
+            //Si lo de dentro de los paréntesis tiene llaves o corchetes no se valida
+            if (palabra.includes("}") || palabra.includes("{") || palabra.includes("]") || palabra.includes("[")) {
+                validada = false;
+            }
+            else {
+                //Si de dentro de los paréntesis es palabra sin llaves y corchetes se valida
+                validada = true;
+            }
+        }
+        else {
+            //Si no tiene nada lo que hay entre los paréntesis no se valida
+            validada = false;
+        }
+    }
+    else {
+        //Si no se cierra el paréntesis o no tiene paréntesis no se valida
+        validada = false;
+    }
+    return validada;
+};
+console.log(validar("() bici"));
