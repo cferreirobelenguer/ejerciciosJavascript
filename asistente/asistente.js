547affb63760db14f12ccdfff42223e8aa715ca2
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-07-13 21:01:28 +0200

    descripcion asistente

--- asistente/asistente.js
+++ asistente/asistente.js
@@ -1,3 +1,16 @@
+/**
+ * Bob es un adolescente indiferente. En la conversación, sus respuestas son muy limitadas.
+
+Bob responde 'Claro'. si le haces una pregunta, como "¿Cómo estás?".
+
+Él responde '¡Vaya, relájate!' si le GRITAS (en mayúsculas).
+
+Él responde '¡Cálmate, sé lo que estoy haciendo!' si le gritas una pregunta.
+
+Él dice 'Bien. ¡Sé así! si te diriges a él sin decir nada.
+
+Él responde 'Lo que sea'. a cualquier otra cosa
+ */
 setTalk = (data) => {
     if (data) {
         if (data.includes('?') && data === data.toUpperCase()) {
